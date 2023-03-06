a5d069a1ba765c636711995d5eaf7a1c63cfda74
JAYARIKAHS <126595496+JAYARIKAHS@users.noreply.github.com>
2023-03-06 16:26:35 -0600

    Update CheckLastLogonTime.ps1

--- CheckLastLogonTime.ps1
+++ CheckLastLogonTime.ps1
@@ -1,3 +1,6 @@
+# ------------------------------------------
+# GET LAST LOGON TIME FROM LIST OF USERNAMES
+# ------------------------------------------
 # <file_name>.txt should contain AD User IDs on each line
 
 $USERS = Get-Content "C:\Temp\<file_name>.txt"
@@ -5,6 +8,22 @@ $USERS = Get-Content "C:\Temp\<file_name>.txt"
 foreach ($USER in $USERS)
 {
 	$GETUSER = Get-ADUser -Identity $USER -Properties "lastLogonTimestamp"
+	$LASTLOGON = [DateTime]::FromFileTime($GETUSER.lastLogonTimestamp)
+	$GETUSER | Select-Object Name, @{name="LastLogon";expression={$LASTLOGON}}
+}
+
+
+
+# ----------------------------------------------
+# GET LAST LOGON TIME FROM LIST OF DISPLAY NAMES
+# ----------------------------------------------
+# <file_name>.txt should contain name of users on each line
+
+$USERS = Get-Content "C:\Temp\<file_name>.txt"
+ 
+foreach ($USER in $USERS)
+{
+	$GETUSER = Get-ADUser -Filter {Name -like $name} -Properties "lastLogonTimestamp"
 	$LASTLOGON = [DateTime]::FromFileTime($GETUSER.lastLogonTimestamp)
 	$GETUSER | Select-Object Name, @{name="LastLogon";expression={$LASTLOGON}}
 }
