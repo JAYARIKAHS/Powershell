e78e84a928ac86e2c26fc0840421f461c9582aa1
JAYARIKAHS <126595496+JAYARIKAHS@users.noreply.github.com>
2023-03-07 13:42:56 -0600

    Create CheckPortIsOpen.ps1

---
+++ CheckPortIsOpen.ps1
@@ -0,0 +1,4 @@
+Test-NetConnection [IP address or hostname] -Port [port number]
+
+# If the port is open, you will see a message that says "TcpTestSucceeded : True".
+# If the port is closed, you will see a message that says "TcpTestSucceeded : False".
